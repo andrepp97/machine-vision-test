d27ddecd07d6554bca3a77dc81b2b4b77829853e
Andre Putera Pratama <andre.putera@majoo.id>
2023-09-01 09:50:38 +0700

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,13 +2,36 @@ import PropTypes from 'prop-types'
 import ContentLoader from 'react-content-loader'
 
 const Loader = ({ width }) => {
+    const dynamicColumns = () => {
+        if (width >= 1288) {
+            return 4
+        } else if (width < 1288 && width > 975) {
+            return 3
+        } else if (width <= 975 && width > 600) {
+            return 2
+        } else if (width <= 600) {
+            return 1
+        }
+    }
+
+    const dynamicWidth = () => {
+        if (width >= 1288) {
+            return width / 6
+        } else if (width < 1288 && width > 975) {
+            return width / 4
+        } else if (width <= 975 && width > 600) {
+            return width / 3
+        } else if (width <= 600) {
+            return width - 32
+        }
+    }
+
     const rows = 2
-    const columns = width > 960 ? 4 : 3
-    const coverWidth = width > 960 ? width / 6.5 : width / 4
+    const columns = dynamicColumns()
+    const coverWidth = dynamicWidth()
     const coverHeight = 300
     const padding = 32
     const speed = 2
-
     const coverHeightWithPadding = coverHeight + padding
     const coverWidthWithPadding = coverWidth + padding
     const covers = Array(columns * rows).fill(1)
